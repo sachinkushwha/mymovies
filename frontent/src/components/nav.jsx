699acb948fadf27ae615072d5806984c09373aa0
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2025-07-31 01:14:33 +0530

    Update nav.jsx

--- frontent/src/components/nav.jsx
+++ frontent/src/components/nav.jsx
@@ -40,7 +40,7 @@ const handleprofile=()=>{
                         className="w-10 h-10 rounded-full bg-blue-600 text-white text-xl flex items-center justify-center shadow hover:bg-blue-700 transition"
                         onClick={() => setIsOpen(!isOpen)}
                     >
-                        {user?.[0] || user?.charAt(0) }
+                        {(user?.[0] || user?.charAt(0) || '').toUpperCase()}
                     </button>
                     
 
@@ -53,7 +53,7 @@ const handleprofile=()=>{
                         <>
                             <Link onClick={handlelogout}>Logout</Link>
                             <Link to="/upload">Upload</Link>
-                            <span onClick={handleprofile} className="w-10 h-10 rounded-full bg-blue-600 text-white text-xl flex items-center justify-center shadow hover:bg-blue-700 transition">{user.at(0).toUpperCase()}</span>
+                            <span onClick={handleprofile} className="w-10 h-10 rounded-full bg-blue-600 text-white text-xl flex items-center justify-center shadow hover:bg-blue-700 transition">{(user?.[0] || user?.charAt(0) || '').toUpperCase()}</span>
                         </>
                     ) : (
                         <Link to="/login">Login</Link>
